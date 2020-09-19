c59feaee314436f7b4bdf9c78f04efdb0eca4056
Krutika <krutikavk@gmail.com>
2020-09-19 12:35:52 -0700

    Hit endpoint

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (request, response) => {
+	response.send('Hit /');
 	/*
 	fetch(url)
 			.then(response => {
